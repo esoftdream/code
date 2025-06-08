e7d5e2b078ad95d536fc37263fb8afcfb12e92ca
Toto <totoprayogo1916@gmail.com>
2025-06-08 11:39:43 +0700

    fix: perbaiki parameter dan validasi pada metode code dan generate

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -6,8 +6,12 @@ use CodeIgniter\Config\BaseService;
 
 class Services extends BaseService
 {
-    public static function code()
+    public static function code($getShared = true)
     {
+        if ($getShared) {
+            return static::getSharedInstance('code');
+        }
+
         return new \Esoftdream\Code\Generator();
     }
 }
